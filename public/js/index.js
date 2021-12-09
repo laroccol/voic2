922e8bd0beeb2a6086809573ecdd16d9149adaac
laroccol <50590312+laroccol@users.noreply.github.com>
2021-12-08 22:34:31 -0700

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -17,7 +17,7 @@ window.onload = (e) => {
   mainFunction(1000);
 };
 
-var socket = io("ws://localhost:3000");
+var socket = io();
 socket.emit("userInformation", userStatus);
 
 function mainFunction(time) {
